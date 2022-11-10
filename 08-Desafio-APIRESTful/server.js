0a756817f58e484a92d7bfbf6be151aba5005b43
robertostorino <roberguitarsoul@gmail.com>
2022-11-10 15:35:50 -0300

    APIRESTful iniciando

---
+++ 08-Desafio-APIRESTful/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const { Router } = express; //Hago destructuring de express para poder usar Router, en lugar de express.Router
+
+const app = express();
+const router = Router();
